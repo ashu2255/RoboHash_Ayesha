3cf68b5890d5c5e0c3540720ffddf5734ea2781d
Ayesha Harun Sayyad <ayeshasayyad2255@gmail.com>
2021-10-03 18:43:44 +0530

    fix: changes made in file

--- callback.js
+++ callback.js
@@ -1,33 +1,25 @@
 //callback
-var fs = require('fs');
-const superagent = require('superagent');
+var fs = require("fs");
+const superagent = require("superagent");
 const data = Math.random().toString(36);
 
 function RandomString(data) {
-    console.log(`Random string is :${data}`);
+  console.log(`Random string is :${data}`);
 }
 
 superagent.get(`https://robohash.org/${data}`).end((err, res) => {
+  if (err) {
+    console.log("not able to retive correct breed image", err);
+    return;
+  }
+  console.log(res.request.url);
+  fs.writeFile("./RoboImage.txt", res.request.url, (err, res) => {
     if (err) {
-        console.log("not able to retive correct breed image", err);
-        return;
+      console.log("not able to store the image", err);
+      return;
     }
-    console.log(res.request.url);
-    fs.writeFile("./RoboImage.txt", res.request.url, (err, res) => {
-        if (err) {
-            console.log("not able to store the image", err);
-            return;
-        }
-        console.log("Robo Image saved inside file");
-    })
+    console.log("Robo Image saved inside file");
+  });
+});
 
-
-})
-
-RandomString(data)
-
-
-
-
-
-
+RandomString(data);
