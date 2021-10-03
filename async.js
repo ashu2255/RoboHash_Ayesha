1a790734361711f3cb2cb300f231e9b19bbf2180
Ayesha Harun Sayyad <ayeshasayyad2255@gmail.com>
2021-10-03 18:43:20 +0530

    feat:with async await

--- async.js
+++ async.js
@@ -1,32 +1,32 @@
-var fs = require('fs');
-const superagent = require('superagent');
+var fs = require("fs");
+const superagent = require("superagent");
 
 const data = Math.random().toString(36).substring(2, 7);
 
 function RandomString(data) {
-    console.log(`Random string is :${data}`);
+  console.log(`Random string is :${data}`);
 }
-function writeFilePromise(filelocation, data){
-    return new Promise((resolve, reject) =>{ 
-        fs.writeFile(filelocation,data,(err)=>{
-            if(err){
-                reject("not able to write content in file");
-            }
-            resolve(data);
-        });
+function writeFilePromise(filelocation, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filelocation, data, (err) => {
+      if (err) {
+        reject("not able to write content in file");
+      }
+      resolve(data);
     });
+  });
 }
 
-async function robohash(){
-    try{
-        RandomString(data)
-        const res = await superagent.get(`https://robohash.org/${data}`);
-        console.log(res.request.url);
-        writeFilePromise("./RoboImage.txt",res.request.url)
-        console.log("image saved successfully!")
-    }catch(err){
-        console.log(err);
-    }
+async function robohash() {
+  try {
+    RandomString(data);
+    const res = await superagent.get(`https://robohash.org/${data}`);
+    console.log(res.request.url);
+    writeFilePromise("./RoboImage.txt", res.request.url);
+    console.log("image saved successfully!");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-robohash()
+robohash();
