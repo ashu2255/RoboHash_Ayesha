0188133374a340d89414e8bed805e567a0dda452
Ayesha Harun Sayyad <ayeshasayyad2255@gmail.com>
2021-10-03 18:44:17 +0530

    feat:with promise

--- promise.js
+++ promise.js
@@ -1,35 +1,31 @@
-var fs = require('fs');
-const superagent = require('superagent');
+var fs = require("fs");
+const superagent = require("superagent");
 
 const data = Math.random().toString(36).substring(2, 7);
 
-
 function writeFilePromise(filelocation, data) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filelocation, data, (err) => {
-            if (err) {
-                reject("not able to write content in file");
-            }
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filelocation, data, (err) => {
+      if (err) {
+        reject("not able to write content in file");
+      }
+      resolve(data);
     });
+  });
 }
-
 
 function RandomString(data) {
-    console.log(`Random string is :${data}`);
+  console.log(`Random string is :${data}`);
 }
-superagent.get(`https://robohash.org/${data}`)
-.end((err,res)=>{
-	console.log(res.request.url)
-	return writeFilePromise("./RoboImage.txt",res.request.url)
+superagent.get(`https://robohash.org/${data}`).end((err, res) => {
+  console.log(res.request.url);
+  return writeFilePromise("./RoboImage.txt", res.request.url)
+    .then(() => {
+      console.log("sucessfully written contents inside the file");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-.then(()=>{
-	console.log('sucessfully written contents inside the file')
-})
-.catch((err)=>{
-	console.log(err)
-})
-})
-
-RandomString(data)
+RandomString(data);
